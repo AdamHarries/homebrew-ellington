8d83bf4f57800d22b008178d7a7c2ca02bd0a743
Adam Harries <harries.adam@gmail.com>
2018-07-27 21:43:01 +0100

    fuck fuck

--- Formula/ellington.rb
+++ Formula/ellington.rb
@@ -8,7 +8,7 @@ class Ellington < Formula
   depends_on "id3v2"
   depends_on "mp4v2"
   def install
-    system "cargo", "build", "--release"
+    system "cargo build --release"
     bin.install "target/release/ellington"
   end
 
